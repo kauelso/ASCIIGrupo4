859372d902a1c8759f695095205ba8ebdf084f0f
Kaue Oliveira <kauelucas@terra.com.br>
2020-07-09 12:05:47 -0300

    Athenticate post

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -24,5 +25,19 @@ router.post('/register', async (req, res) => {
     });
   }
 });
+
+router.post('/authenticate',async (req,res)=>{
+  const {email , password} = req.body;
+  const user = await (await User.findOne({email})).isSelected('+password');
+
+  if(!user){
+    return res.status(400).send({error: "user not found"});}
+
+  if(!await bcrypt.compare(password,user.password)){
+    return res.status(400).send({error: "Invalid password"});}
+  
+  res.send({user});
+
+})
 
 module.exports = (app) => app.use('/api/auth', router);
