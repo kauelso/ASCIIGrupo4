e17acd1c448587d8cc48c2ba60c2d9fbb5881615
Adiel Pereira <adielp16@gmail.com>
2020-07-13 12:38:04 -0300

    add rota de alteracao de senha

--- api/src/app/controllers/userController.js
+++ api/src/app/controllers/userController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const authMiddleware = require('../middlewares/auth');
 router.use(authMiddleware);//as rotas abaixo estao protegidas por autenticacao
 
@@ -19,6 +20,50 @@ router.get('/user', async(req, res) => { // listar usuario
   return res.status(200).json({
     user
   });
+});
+
+router.put('/user/change_password/:id', async(req, res) => { // atualizar senha
+  let {email, oldPassword, newPassword } = req.body;
+  try {
+
+    console.log(oldPassword, newPassword);
+    if(!oldPassword || !newPassword) // verificar se foi passado as senhas
+      return res.status(401).json({
+        error: "falha na alteração da senha, tente novamente"
+      });
+    oldPassword = oldPassword.toString();
+    newPassword = newPassword.toString();
+
+    if(req.userId !== req.params.id) // verificar se o usuario esta tentando a alterar senha de outro usuario
+      return res.status(401).json({
+        error: "falha na alteração da senha, tente novamente"
+      });
+
+    const user = await User.findById(req.params.id)
+      .select('+password');
+
+    if(!user)
+      return res.status(401).json({
+        error: "falha na alteração da senha, tente novamente"
+      });  
+
+    // conferir se a senha antiga esta correta  
+    if(!await bcrypt.compare(oldPassword, user.password)){
+      return res.status(401).json({
+        error: "falha na alteração da senha, tente novamente"
+      });
+    }
+
+    user.password = newPassword;
+    await user.save();
+    user.password = undefined; // para n retornar a senha pro frontend
+    return res.json({ user });
+  }catch(err){
+    console.log(err)
+    res.status(401).json({
+      error: "falha na alteração da senha, tente novamente"
+    });
+  }
 });
 
 router.put('/user/:id', async(req, res) => { // atualizar usuario
@@ -50,5 +95,6 @@ router.put('/user/:id', async(req, res) => { // atualizar usuario
     });
   }
 });
+
 
 module.exports = (app) => app.use('/api/auth', router);
