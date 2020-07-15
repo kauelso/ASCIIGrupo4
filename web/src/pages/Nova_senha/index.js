9e84efef83e470b213ca99c57dce21c741b7b4f4
Joaovitorufu <Joaovitorufu@gmail.com>
2020-07-15 15:46:50 -0300

    alteração nova senha

--- web/src/pages/Nova_senha/index.js
+++ web/src/pages/Nova_senha/index.js
@@ -6,15 +6,15 @@ import logo from '../../assets/logo.svg';
 const Nova_senha =() =>{
 
     return(
-        <div className="body">
+        <div className="nova-senha-container">
             <div className="senha-side">
             <img id="icon-size" src={logo} alt="Plantfolio Ascii"/>
             <h1>Plantfolio<br></br>Ascii</h1>
             </div>
             <div className="dados-senha">
                 <h2>Crie uma nova <br></br>senha</h2>
-                <imput type="password" placeholder="Nova Senha"></imput>
-                <imput type="password"  placeholder="Confirme a Nova Senha"></imput>
+                <input type="password" placeholder="Nova Senha"></input>
+                <input type="password"  placeholder="Confirme a Nova Senha"></input>
             </div>
         </div>
         
