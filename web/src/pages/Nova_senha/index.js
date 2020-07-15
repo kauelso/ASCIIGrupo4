aec68819bee06557c7d94dfd729758bda66f573c
Joaovitorufu <Joaovitorufu@gmail.com>
2020-07-15 15:01:16 -0300

    modificando Nova_senha

--- web/src/pages/Nova_senha/index.js
+++ web/src/pages/Nova_senha/index.js
@@ -6,14 +6,15 @@ import logo from '../../assets/logo.svg';
 const Nova_senha =() =>{
 
     return(
-        <div>
+        <div className="body">
             <div className="senha-side">
             <img id="icon-size" src={logo} alt="Plantfolio Ascii"/>
             <h1>Plantfolio<br></br>Ascii</h1>
             </div>
             <div className="dados-senha">
                 <h2>Crie uma nova <br></br>senha</h2>
-                
+                <imput type="password" placeholder="Nova Senha"></imput>
+                <imput type="password"  placeholder="Confirme a Nova Senha"></imput>
             </div>
         </div>
         
